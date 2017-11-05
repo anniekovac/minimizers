27c6afc212cc7d386a115324590293cfe5211599
Roko Lisica <roko.lisica@fer.hr>
2017-11-05 21:33:58 +0100

    Added and implemented Sequence class.

---
+++ java/src/hr/fer/bioinf/minimizer/sequence/Sequence.java
@@ -0,0 +1,13 @@
+package hr.fer.bioinf.minimizer.sequence;
+
+public class Sequence {
+
+    private final String name;
+    private final String string;
+
+    public Sequence(String name, String string) {
+        this.name = name;
+        this.string = string;
+    }
+
+}
