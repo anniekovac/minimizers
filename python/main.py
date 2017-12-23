f64e1d3965df6f9b0e483bce39dc1bf491cb7cc9
anniekovac <anniekovac95@gmail.com>
2017-12-23 23:54:12 +0100

    Implementing some changes made for considering window sizes

--- python/main.py
+++ python/main.py
@@ -1,11 +1,11 @@
-def main():
-	#my_string = "2310343"
-	#k = 3
-	my_string = "426472814751"
-	k = 7
-	window_size = 5
-
-	my_string_copy = my_string
+def main(input_string, k, window_size):
+	"""
+	:param input_string: str (string in which you want to find minimizers in) 
+	:param k: int (size of kmers)
+	:param window_size: int (size of window which we consider)
+	:return: 
+	"""
+	my_string_copy = input_string
 	kmers = []
 	i = 0
 	while my_string_copy:
@@ -18,14 +18,28 @@ def main():
 		i += 1
 
 	minimizers = dict()
-	minimizer = None
-	for position, kmer in enumerate(kmers):
-		if minimizer is None or kmer < minimizer:
-			minimizer, position = kmer, position
-	minimizers[minimizer] = position
-	print("MINIMIZER:", minimizer)
-
+	window_counter = 0
+	for brojac in range(0, 15):
+		kmers_in_window = kmers[window_counter:window_counter + window_size]
+		minimizer = None
+		for position, kmer in enumerate(kmers_in_window):
+			if minimizer is None or kmer < minimizer:
+				minimizer, position = kmer, position
+		minimizers[minimizer] = position
+		window_counter += 1
+		print("MINIMIZER:", minimizer)
+	print(minimizers)
 
 
 if __name__ == "__main__":
-	main()
+	#my_string = "2310343"
+	#k = 3
+
+	#my_string = "426472814751"
+	#k = 7
+
+	my_string = "231032101233101"
+	k = 3
+	window_size = 6
+
+	main(my_string, k, window_size)
