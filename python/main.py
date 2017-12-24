718aed4ada604f5032d2c43b25e82cacceccb3df
anniekovac <anniekovac95@gmail.com>
2017-12-24 12:50:17 +0100

    fixed first TODO: fixed returning indexes of kmers

--- python/main.py
+++ python/main.py
@@ -1,9 +1,15 @@
-# TODO: fix indexes in returned kmers (given string is input string and it always begins
-# 		from the zero (0)
-
 # TODO: fix returning kmers for substring 123310 - WHATS WRONG ???
 
-def return_kmers(input_string, k):
+
+def return_kmers(input_string, k, index_from_beginning):
+	"""
+	Returns kmers from given input string.
+	Length of returned kmers is k.
+	:param input_string: str (string in which you want to find kmers)
+	:param k: int (length of kmers you want to extract from your string)
+	:param index_from_beginning: int (index from which input_string starts in original string)
+	:return: list [(str, int), (str, int) ...] where str is kmer and int is its position in string
+	"""
 	my_string_copy = input_string
 	kmers = []
 	i = 0
@@ -11,7 +17,7 @@ def return_kmers(input_string, k):
 		kmer = input_string[i:i+k]
 		if len(kmer) < k:
 			break
-		kmers.append((kmer, i))
+		kmers.append((kmer, index_from_beginning + i))
 		my_string_copy = my_string_copy.split(my_string[i:i+k], 1)[-1]
 		i += 1
 	return kmers
@@ -30,7 +36,7 @@ def main(input_string, k, window_size):
 		substring = input_string[window_counter:window_counter + window_size]
 		if len(substring) < window_size:
 			break
-		kmers_in_window = return_kmers(substring, k)
+		kmers_in_window = return_kmers(substring, k, window_counter)
 		print("substring", substring, "kmers in window:", kmers_in_window)
 		minimizer = None
 		for kmer in kmers_in_window:
