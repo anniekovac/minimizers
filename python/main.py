65a5a80c79b18bfb0eb580bcfab97fcade85b910
anniekovac <anniekovac95@gmail.com>
2017-12-24 15:18:37 +0100

    Added tests, moved to new module

--- python/main.py
+++ python/main.py
@@ -31,9 +31,6 @@ def return_minizers(input_string, k, window_size):
 	window_counter = 0
 	while True:
 		substring = input_string[window_counter:window_counter + window_size]
-		# print("substring:", substring)
-		# print("window_counter:", window_counter)
-		# print("window_size:", window_size)
 		if len(substring) < window_size:
 			break
 		kmers_in_window = return_kmers(substring, k, window_counter)
@@ -44,45 +41,3 @@ def return_minizers(input_string, k, window_size):
 		minimizers[minimizer] = position
 		window_counter += 1
 	return minimizers
-
-
-def test1():
-	my_string = "2310343"
-	k = 3
-	window_size = len(my_string)
-	minimizers_should_be = {"034": 3}
-	minimizers_are = return_minizers(my_string, k, window_size)
-	assert minimizers_are == minimizers_should_be, """Test test1 failed! Minimizers that code returned:
-													{}
-													Minimizers that code should have returned:
-													{}""".format(minimizers_are, minimizers_should_be)
-
-
-def test2():
-	my_string = "426472814751"
-	k = 7
-	window_size = len(my_string)
-	minimizers_should_be = {"2647281":1}
-	minimizers_are = return_minizers(my_string, k, window_size)
-	assert minimizers_are == minimizers_should_be, """Test test2 failed! Minimizers that code returned:
-													{}
-													Minimizers that code should have returned:
-													{}""".format(minimizers_are, minimizers_should_be)
-
-
-def test3():
-	my_string = "231032101233101"
-	k = 3
-	window_size = 6
-	minimizers_should_be = {'032': 3, '012': 7, '123': 8, '101': 12}
-	minimizers_are = return_minizers(my_string, k, window_size)
-	assert minimizers_are == minimizers_should_be, """Test test3 failed! Minimizers that code returned:
-													{}
-													Minimizers that code should have returned:
-													{}""".format(minimizers_are, minimizers_should_be)
-
-
-if __name__ == "__main__":
-
-	test1()
-	test2()
---
+++ python/tests.py
@@ -0,0 +1,61 @@
+from main import return_minizers, return_kmers
+
+
+def test1_return_minimizers():
+	my_string = "2310343"
+	k = 3
+	window_size = len(my_string)
+	minimizers_should_be = {"034": 3}
+	minimizers_are = return_minizers(my_string, k, window_size)
+	assert minimizers_are == minimizers_should_be, """Test test1 failed! Minimizers that code returned:
+													{}
+													Minimizers that code should have returned:
+													{}""".format(minimizers_are, minimizers_should_be)
+	print("Test test1_return_minimizers OK")
+
+
+def test2_return_minimizers():
+	my_string = "426472814751"
+	k = 7
+	window_size = len(my_string)
+	minimizers_should_be = {"2647281":1}
+	minimizers_are = return_minizers(my_string, k, window_size)
+	assert minimizers_are == minimizers_should_be, """Test test2 failed! Minimizers that code returned:
+													{}
+													Minimizers that code should have returned:
+													{}""".format(minimizers_are, minimizers_should_be)
+	print("Test test2_return_minimizers OK")
+
+
+def test3_return_minimizers():
+	my_string = "231032101233101"
+	k = 3
+	window_size = 6
+	minimizers_should_be = {'032': 3, '012': 7, '123': 8, '101': 12}
+	minimizers_are = return_minizers(my_string, k, window_size)
+	assert minimizers_are == minimizers_should_be, """Test test3 failed! Minimizers that code returned:
+													{}
+													Minimizers that code should have returned:
+													{}""".format(minimizers_are, minimizers_should_be)
+	print("Test test3_return_minimizers OK")
+
+
+def test1_return_kmers():
+	my_string = "2310343"
+	k = 3
+	window_counter = 0
+	kmers = return_kmers(my_string, k, window_counter)
+	kmers_should_be = [('231', 0), ('310', 1), ('103', 2), ('034', 3), ('343', 4)]
+	assert kmers == kmers_should_be, """Test test3 failed! KMERs that code returned:
+										{}
+										KMERS that code should have returned:
+										{}""".format(kmers, kmers_should_be)
+	print("Test test1_return_kmers OK")
+
+
+if __name__ == "__main__":
+
+	test1_return_minimizers()
+	test2_return_minimizers()
+	test3_return_minimizers()
+	test1_return_kmers()
