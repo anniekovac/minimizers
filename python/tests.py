f350c5efb6f72292dbcee17dd325b44ad00320b9
anniekovac <anniekovac95@gmail.com>
2017-12-25 18:48:35 +0100

    Updated tests and added new tests that questions returned positions with minimizers

--- python/tests.py
+++ python/tests.py
@@ -1,4 +1,5 @@
 from main import return_minizers, return_kmers
+from pprint import pprint as pp
 
 
 def test1_return_minimizers():
@@ -7,6 +8,9 @@ def test1_return_minimizers():
 	window_size = len(my_string)
 	minimizers_should_be = {"034": 3}
 	minimizers_are = return_minizers(my_string, k, window_size)
+	keys = [key for key in minimizers_are]
+	for key in keys:
+		minimizers_are[key] = minimizers_are[key][0].position
 	assert minimizers_are == minimizers_should_be, """Test test1 failed! Minimizers that code returned:
 													{}
 													Minimizers that code should have returned:
@@ -20,6 +24,9 @@ def test2_return_minimizers():
 	window_size = len(my_string)
 	minimizers_should_be = {"2647281":1}
 	minimizers_are = return_minizers(my_string, k, window_size)
+	keys = [key for key in minimizers_are]
+	for key in keys:
+		minimizers_are[key] = minimizers_are[key][0].position
 	assert minimizers_are == minimizers_should_be, """Test test2 failed! Minimizers that code returned:
 													{}
 													Minimizers that code should have returned:
@@ -33,6 +40,9 @@ def test3_return_minimizers():
 	window_size = 6
 	minimizers_should_be = {'032': 3, '012': 7, '123': 8, '101': 12}
 	minimizers_are = return_minizers(my_string, k, window_size)
+	keys = [key for key in minimizers_are]
+	for key in keys:
+		minimizers_are[key] = minimizers_are[key][0].position
 	assert minimizers_are == minimizers_should_be, """Test test3 failed! Minimizers that code returned:
 													{}
 													Minimizers that code should have returned:
@@ -66,9 +76,41 @@ def test2_return_kmers():
 	print("Test test1_return_kmers OK")
 
 
+def test_positions():
+
+	string_of_choice = "AAGATGGCTAAGCAAGATTA"
+	mini_in_string = return_minizers(string_of_choice, 5, 6)
+
+	returned_positions = [item.position for item in mini_in_string["AAGAT"]]
+	positions_should_be = [0, 13]
+	assert returned_positions == positions_should_be, """Wrong positions returned for 'AAGAT',
+														positions returned: {}, 
+														positions should be: {}""".format(returned_positions, positions_should_be)
+
+	returned_positions = [item.position for item in mini_in_string["AGATG"]]
+	positions_should_be = [1]
+	assert returned_positions == positions_should_be, """Wrong positions returned for 'AGATG',
+														positions returned: {}, 
+														positions should be: {}""".format(returned_positions, positions_should_be)
+
+	returned_positions = [item.position for item in mini_in_string["ATGGC"]]
+	positions_should_be = [3]
+	assert returned_positions == positions_should_be, """Wrong positions returned for 'ATGGC',
+														positions returned: {}, 
+														positions should be: {}""".format(returned_positions, positions_should_be)
+
+	returned_positions = [item.position for item in mini_in_string["AAGCA"]]
+	positions_should_be = [9]
+	assert returned_positions == positions_should_be, """Wrong positions returned for 'AAGCA',
+														positions returned: {}, 
+														positions should be: {}""".format(returned_positions, positions_should_be)
+
+	print("Test test_positions OK")
+
 if __name__ == "__main__":
 
 	test1_return_minimizers()
 	test2_return_minimizers()
 	test3_return_minimizers()
 	test1_return_kmers()
+	test_positions()
