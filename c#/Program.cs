ce605a5699f10837c4f43c436fbdc6a195a35959
Matej Erceg <mat.erceg@gmail.com>
2018-01-14 12:29:46 +0100

    initial c# commit

---
+++ c#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinimizersCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
