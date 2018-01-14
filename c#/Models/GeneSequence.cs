d0ffafd17bbb834098d258824f7c4996c56c3888
Matej Erceg <mat.erceg@gmail.com>
2018-01-14 23:26:30 +0100

    Added model classes

---
+++ c#/Models/GeneSequence.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MinimizersCore.Models
+{
+    public class GeneSequence
+    {
+        private string name;
+        private string body;
+
+        public string Name { get => name; set => name = value; }
+        public string Body { get => body; set => body = value; }
+
+        /// <summary>
+        /// Constructor for GeneSequence with given name and body
+        /// </summary>
+        /// <param name="name">The name of the sequence</param>
+        /// <param name="body">The content of the sequence that is to be analyzed for minimizers</param>
+        public GeneSequence(string name, string body)
+        {
+            Name = name;
+            Body = body;
+        }
+        
+        //?
+        public override bool Equals(Object o)
+        {
+            if (this == o) return true;
+            if (o == null || this.GetType() != o.GetType()) return false;  // GetType in c# is the same as getClass() in java, returns runtime type
+
+            GeneSequence sequence = (GeneSequence)o;
+
+            if (Name != sequence.Name) return false;
+            return Body.Equals(sequence.Body);
+        }
+
+        //?
+        public override int GetHashCode()
+        {
+            int result = Name.GetHashCode();
+            result = 31 * result + Body.GetHashCode();
+            return result;
+        }
+    }
+}
---
+++ c#/Models/Minimizer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MinimizersCore.Models
+{
+    public class Minimizer
+    {
+        private string minimizerString;
+        private GeneSequence sequence;
+        private int position;
+
+        public string MinimizerString { get => minimizerString; set => minimizerString = value; }
+        internal GeneSequence Sequence { get => sequence; set => sequence = value; }
+        public int Position { get => position; set => position = value; }
+
+
+        /// <summary>
+        /// Constructs a minimizer with given parameters
+        /// </summary>
+        /// <param name="minimizerString">One extracted minimizer substring</param>
+        /// <param name="sequence">Originating sequence for the minimizer substring</param>
+        /// <param name="position">Position in sequence where the minimizer substring appears</param>
+        public Minimizer(string minimizerString, GeneSequence sequence, int position)
+        {
+            MinimizerString = minimizerString;
+            Sequence = sequence;
+            Position = position;
+        }
+
+              
+        public override string ToString()
+        {
+            return MinimizerString + " on position " + Position.ToString() + " in " + Sequence.Name;
+        }
+
+        //?
+        public override bool Equals(Object other)
+        {
+            if (this == other) return true;
+            if (other == null /*|| this.getType() != other.getType()*/) return false;
+
+            Minimizer minimizer = (Minimizer)other;
+            return (Sequence == minimizer.Sequence && Position == minimizer.Position);
+        }
+    }
+}
