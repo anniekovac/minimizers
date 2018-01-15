bf1ce51bfacfe7290357fb6ce3108002ebe48423
anniekovac <anniekovac95@gmail.com>
2018-01-15 11:33:15 +0100

    Added program for searching query from hashtable - python

---
+++ python/MinimizerHashTableQuery.py
@@ -0,0 +1,30 @@
+import sys
+from main import return_minizers, parse_fasta_file, Minimizer
+
+
+def load_hashtable(hash_file):
+	"""
+	Function that will receive data from minimizer_table
+	and write it in the file on disk. This will include
+	parsing of existing data in dictionary minimizer_table
+	in specific format.
+	:param minimizer_table: dict (dictionary of minimizers)
+	:param hash_file: str (path to the file in which you want to save hash table
+	:return: 
+	"""
+	minimizer_table = dict()
+	with open(hash_file, "r") as file:
+		for line in file:
+			parsed_line = line.split(":")
+			key = parsed_line[0]
+			positions = parsed_line[1]
+			mini = Minimizer(positions, key)
+			minimizer_table[key] = mini
+	return minimizer_table
+
+if __name__ == "__main__":
+	arguments = ''.join(sys.argv[1:])
+	hash_file = arguments
+	minimizer_dictionary = load_hashtable(hash_file)
+	print(minimizer_dictionary["TTTCA"].position)
+	# TTTCA:[4, 5795, 10555, 11256, 11399]
