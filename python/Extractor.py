7f6c1a306be3fa1c0658fcc4b22fbef5722ef715
anniekovac <anniekovac95@gmail.com>
2018-01-16 17:32:26 +0100

    Small changes in extractor made for testing - output is now written in out.txt not in the standard output - python

--- python/Extractor.py
+++ python/Extractor.py
@@ -3,10 +3,13 @@ from main import return_minizers
 
 if __name__ == "__main__":
 	a = ''.join(sys.argv[1:])
-	print(a)
-	minimizers = return_minizers(a, 4, 4)
-	for key, minimizer in minimizers.items():
-		if isinstance(minimizer, list):
-			print("Position of minimizer {} is: {}".format(key, [item.position for item in minimizer]))
-		else:
-			print("Position of minimizer {} is: {}".format(key, minimizer.position))
+	# print(a)
+	minimizers = return_minizers(a, 4, 5)
+	with open("out.txt", "w") as out:
+		for key, minimizer in minimizers.items():
+			if isinstance(minimizer, list):
+				# print("Position of minimizer {} is: {}".format(key, [item.position for item in minimizer]))
+				out.write("Position of minimizer {} is: {}".format(key, [item.position for item in minimizer]))
+			else:
+				out.write("Position of minimizer {} is: {}".format(key, minimizer.position))
+				# print("Position of minimizer {} is: {}".format(key, minimizer.position))
