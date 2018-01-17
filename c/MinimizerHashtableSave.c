5b1e252957dc5ae12b2e2d5a0d3efc939d05ef08
FilipPolic <filipzd@gmail.com>
2018-01-17 16:04:32 +0100

    MinimizerHashTableSave now extracts minimizers from given fasta file

--- c/MinimizerHashtableSave.c
+++ c/MinimizerHashtableSave.c
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
 	Minimizer *Minims;
 	Minimizer *backMinims;
 	Minimizer lastPrinted;
+	FILE * tmpFile;
 	
 	if (argc != 5) {
 		printf("Invalid number of arguments.\n");
@@ -36,6 +37,7 @@ int main(int argc, char *argv[])
  
 	fp = fopen(argv[3], "r");
 	outFile = fopen(argv[4], "w");
+	tmpFile = fopen("fasta.tmp", "w");
 	
 	if (fp == NULL)
 		return -1;
@@ -47,7 +49,7 @@ int main(int argc, char *argv[])
 	backMinim = (char *) malloc(2*k*sizeof(char));
 	currentStr = (char *) malloc(2*k*sizeof(char));
 	Minims = (Minimizer *) malloc(100001*sizeof(Minimizer));
- 
+	
 	int state = 0;
 	while ((read = getline(&line, &len, fp)) != -1) {
 		/* Delete trailing newline */
@@ -56,128 +58,150 @@ int main(int argc, char *argv[])
 		/* Handle comment lines*/
 		if (line[0] == '>') {
 			if (state == 1)
-				printf("\n");
-			fprintf(outFile, "%s: \n", line+1);
+				fprintf(tmpFile, "\n");
+			fprintf(tmpFile, "%s:", line+1);
 			state = 1;
 		} else {
 			/* Print everything else */
-			//printf("%s", line);
-			targetStr = line;
-			totalLength = strlen(targetStr);
-			
-			targetStrPtr = targetStr;
-			strncpy(Minim, targetStr, k);
-			Minim[k] = '\0';
-			
-			minimIndex = 0;
-			strcpy(Minims[minimIndex].mmStr, Minim);
-			Minims[minimIndex].pos = (long int) targetStrPtr - (long int) targetStr;
-			
-			numOfKmers = 1;
-			
-			while ( numOfKmers <= w-1 ) {
+			fprintf(tmpFile, "%s", line);
+		}
+	}
+	fprintf(tmpFile, "\n");
+ 
+	rewind(tmpFile);
+	fclose(tmpFile);
+	tmpFile = fopen("fasta.tmp", "r");
+	while ((read = getline(&line, &len, tmpFile)) != -1) {
+		while (line[0] != ':') {
+			fprintf(outFile, "%c", line[0]);
+			line++;
+		}
+		fprintf(outFile, "%c", line[0]);
+		line++;
+		fprintf(outFile, "\n");
+		targetStr = line;
+		totalLength = strlen(line)-1;
+		
+		// Extract beginning minimizers
+	
+		targetStrPtr = targetStr;
+		strncpy(Minim, targetStr, k);
+		Minim[k] = '\0';
 
-				targetStrPtr++;
-				strncpy(currentStr, targetStrPtr, k);
+		minimIndex = 0;
+		strcpy(Minims[minimIndex].mmStr, Minim);
+		Minims[minimIndex].pos = (long int) targetStrPtr - (long int) targetStr;
+
+		numOfKmers = 1;
+
+		while ( numOfKmers <= w-1 ) {
+
+			targetStrPtr++;
+			strncpy(currentStr, targetStrPtr, k);
+			currentStr[k] = '\0';
+			if (strcmp(Minim, currentStr) > 0) {
+				strcpy(Minim, currentStr);
+				Minim[k] = '\0';
+				minimIndex++;
+				strcpy(Minims[minimIndex].mmStr, Minim);
+				Minims[minimIndex].pos = (long int) targetStrPtr - (long int) targetStr;
+			}
+			numOfKmers++;
+		}
+
+		// Extract internal minimizers
+		
+		targetStrPtr = targetStr;
+
+		while (strlen(targetStrPtr)-1 >= (w + k - 1)) {
+			strncpy(Minim, targetStrPtr, k);
+			minimPos = (long int) targetStrPtr - (long int) targetStr;
+			Minim[k] = '\0';
+			numOfKmers = 0;
+			i = 0;
+			while (numOfKmers < w) {
+				strncpy(currentStr, &targetStrPtr[i], k);
 				currentStr[k] = '\0';
 				if (strcmp(Minim, currentStr) > 0) {
 					strcpy(Minim, currentStr);
 					Minim[k] = '\0';
-					minimIndex++;
-					strcpy(Minims[minimIndex].mmStr, Minim);
-					Minims[minimIndex].pos = (long int) targetStrPtr - (long int) targetStr;
+					minimPos = (long int) &targetStrPtr[i] - (long int) targetStr;
 				}
 				numOfKmers++;
+				i++;
 			}
-			
-			targetStrPtr = targetStr;
-
-			while (strlen(targetStrPtr) >= (w + k - 1)) {
-				strncpy(Minim, targetStrPtr, k);
-				minimPos = (long int) targetStrPtr - (long int) targetStr;
-				Minim[k] = '\0';
-				numOfKmers = 0;
-				i = 0;
-				while (numOfKmers < w) {
-					strncpy(currentStr, &targetStrPtr[i], k);
-					currentStr[k] = '\0';
-					if (strcmp(Minim, currentStr) > 0) {
-						strcpy(Minim, currentStr);
-						Minim[k] = '\0';
-						minimPos = (long int) &targetStrPtr[i] - (long int) targetStr;
-					}
-					numOfKmers++;
-					i++;
-				}
-				if ((strcmp(Minim, Minims[minimIndex].mmStr) != 0) ||
-					(minimPos != Minims[minimIndex].pos)) {
-					minimIndex++;
-					strcpy(Minims[minimIndex].mmStr, Minim);
-					Minims[minimIndex].pos = minimPos;
-				}
-				targetStrPtr++;
-			}
-			
-			targetStrPtr = &targetStr[totalLength-1-k+1];
-
-			backMinims = (Minimizer *) malloc(2 * (w - 1) * sizeof(Minimizer));
-			strncpy(backMinim, targetStrPtr, k);
-			backMinim[k] = '\0';
-			minimPos = (long int) targetStrPtr - (long int) targetStr;
-			backMinimIndex = 0;
-			backMinims[backMinimIndex].pos = minimPos;
-			strcpy(backMinims[backMinimIndex].mmStr, backMinim);
-
-			j = 0;
-			while (strlen(targetStrPtr) < (w + k - 1)) {
-				strncpy(backMinim, targetStrPtr, k);
-				minimPos = (long int) targetStrPtr - (long int) targetStr;
-				backMinim[k] = '\0';
-				numOfKmers = 0;
-				i = 0;
-				j++;
-				while (i < j) {
-					strncpy(currentStr, &targetStrPtr[i], k);
-					currentStr[k] = '\0';
-					if (strcmp(backMinim, currentStr) > 0) {
-						strcpy(backMinim, currentStr);
-						backMinim[k] = '\0';
-						minimPos = (long int) &targetStrPtr[i] - (long int) targetStr;
-					}
-					numOfKmers++;
-					i++;
-				}
-				if ((strcmp(backMinim, backMinims[backMinimIndex].mmStr) != 0) ||
-					(minimPos != backMinims[backMinimIndex].pos)) {
-					backMinimIndex++;
-					strcpy(backMinims[backMinimIndex].mmStr, backMinim);
-					backMinims[backMinimIndex].pos = minimPos;
-				}
-				targetStrPtr--;
-			}
-			
-			for (i = backMinimIndex; i >= 0; i--) {
+			if ((strcmp(Minim, Minims[minimIndex].mmStr) != 0) ||
+				(minimPos != Minims[minimIndex].pos)) {
 				minimIndex++;
-				Minims[minimIndex].pos = backMinims[i].pos;
-				strcpy(Minims[minimIndex].mmStr, backMinims[i].mmStr);
+				strcpy(Minims[minimIndex].mmStr, Minim);
+				Minims[minimIndex].pos = minimPos;
 			}
-			
-			strcpy(lastPrinted.mmStr, "+\0");
-			lastPrinted.pos = -1;
-			for (i = 0; i <= minimIndex; i++) {
-				if (strcmp(Minims[i].mmStr, lastPrinted.mmStr) == 0 &&
-					lastPrinted.pos == Minims[i].pos) continue;
-				fprintf(outFile, "Minimizer %s at position %ld\n", Minims[i].mmStr, Minims[i].pos);
-				strcpy(lastPrinted.mmStr, Minims[i].mmStr);
-				lastPrinted.pos = Minims[i].pos;
+			targetStrPtr++;
+		}
+		
+		// Extract end minimizers
+
+		targetStrPtr = &targetStr[totalLength-1-k+1];
+
+		backMinims = (Minimizer *) malloc(2 * (w - 1) * sizeof(Minimizer));
+		strncpy(backMinim, targetStrPtr, k);
+		backMinim[k] = '\0';
+		minimPos = (long int) targetStrPtr - (long int) targetStr;
+		backMinimIndex = 0;
+		backMinims[backMinimIndex].pos = minimPos;
+		strcpy(backMinims[backMinimIndex].mmStr, backMinim);
+
+		j = 0;
+		while (strlen(targetStrPtr)-1 < (w + k - 1)) {
+			strncpy(backMinim, targetStrPtr, k);
+			minimPos = (long int) targetStrPtr - (long int) targetStr;
+			backMinim[k] = '\0';
+			numOfKmers = 0;
+			i = 0;
+			j++;
+			while (i < j) {
+				strncpy(currentStr, &targetStrPtr[i], k);
+				currentStr[k] = '\0';
+				if (strcmp(backMinim, currentStr) > 0) {
+					strcpy(backMinim, currentStr);
+					backMinim[k] = '\0';
+					minimPos = (long int) &targetStrPtr[i] - (long int) targetStr;
+				}
+				numOfKmers++;
+				i++;
 			}
+			if ((strcmp(backMinim, backMinims[backMinimIndex].mmStr) != 0) ||
+				(minimPos != backMinims[backMinimIndex].pos)) {
+				backMinimIndex++;
+				strcpy(backMinims[backMinimIndex].mmStr, backMinim);
+				backMinims[backMinimIndex].pos = minimPos;
+			}
+			targetStrPtr--;
+		}
+
+		// Ensure end minimizers are written in the proper order
+
+		for (i = backMinimIndex; i >= 0; i--) {
+			minimIndex++;
+			Minims[minimIndex].pos = backMinims[i].pos;
+			strcpy(Minims[minimIndex].mmStr, backMinims[i].mmStr);
+		}
+
+		// Print located minimizers while removing duplicates
+		
+		strcpy(lastPrinted.mmStr, "+\0");
+		lastPrinted.pos = -1;
+		for (i = 0; i <= minimIndex; i++) {
+			if (strcmp(Minims[i].mmStr, lastPrinted.mmStr) == 0 &&
+				lastPrinted.pos == Minims[i].pos) continue;
+			fprintf(outFile, "Minimizer %s at position %ld\n", Minims[i].mmStr, Minims[i].pos);
+			strcpy(lastPrinted.mmStr, Minims[i].mmStr);
+			lastPrinted.pos = Minims[i].pos;
 		}
 	}
-	printf("\n");
  
+	fclose(tmpFile);
 	fclose(fp);
 	fclose(outFile);
-	if (line)
-		free(line);
 	return 0;
 }
