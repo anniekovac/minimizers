afe9349f943ada74bf76d39ea757b92e365809c2
anniekovac <anniekovac95@gmail.com>
2018-01-17 16:36:49 +0100

    Changes in arguments for Extractor - python

--- python/Extractor.py
+++ python/Extractor.py
@@ -7,16 +7,20 @@ if __name__ == "__main__":
 	particular string. These minimizers will be shown in 
 	out.txt directory. Example of using this program:
 	
-	python Extractor.py TTGACGTTGCATTACCGG
+	python Extractor.py TTGACGTTGCATTACCGG;k;w
+	
+	python Extractor.py TTGACGTTGCATTACCGG;4;5
 	"""
 	a = ''.join(sys.argv[1:])
-	# print(a)
-	minimizers = return_minizers(a, 4, 5)
+	string_of_choice, k, w = a.split(";")
+	k = int(k)
+	w = int(w)
+	minimizers = return_minizers(string_of_choice, k, w)
 	with open("out.txt", "w") as out:
 		for key, minimizer in minimizers.items():
 			if isinstance(minimizer, list):
 				# print("Position of minimizer {} is: {}".format(key, [item.position for item in minimizer]))
-				out.write("Position of minimizer {} is: {}".format(key, [item.position for item in minimizer]))
+				out.write("Position of minimizer {} is: {}\n".format(key, [item.position for item in minimizer]))
 			else:
-				out.write("Position of minimizer {} is: {}".format(key, minimizer.position))
+				out.write("Position of minimizer {} is: {}\n".format(key, minimizer.position))
 				# print("Position of minimizer {} is: {}".format(key, minimizer.position))
